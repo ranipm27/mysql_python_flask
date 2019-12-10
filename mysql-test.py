2ce7462fe582744c1eed9035166276241af96c12
pmflypallavi <58667171+pmflypallavi@users.noreply.github.com>
2019-12-11 00:47:42 +0530

    Update mysql-test.py

--- mysql-test.py
+++ mysql-test.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'jdbc:mysql://mysql.gamification.svc.cluster.local:3306/mysql'
+app.config['MYSQL_HOST'] = 'jdbc:mysql://mysql.gamification.svc.cluster.local:3306/sampledb'
 app.config['MYSQL_USER'] = 'xxuser'
 app.config['MYSQL_PASSWORD'] = 'welcome1'
 app.config['MYSQL_DB']= 'sampledb'
@@ -10,7 +10,7 @@ mysql = MySQL(app)
 @app.route('/')
 def employees():
 	cur = mysql.connection.cursor()
-	res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM XXXIBM_PRODUCT_STYLE LIMIT 10")
+	res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM XXIBM_PRODUCT_STYLE LIMIT 10")
 	if res > 0:
 		userDetails = cur.fetchall()
 		return render_template('employee.html',userDetails=userDetails)
