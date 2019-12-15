c287feebdc999ce6900cd25fe07b8d598d29006a
ranipm27 <57614108+ranipm27@users.noreply.github.com>
2019-12-15 21:51:05 +0530

    Update mysql-test.py

--- mysql-test.py
+++ mysql-test.py
@@ -2,18 +2,26 @@ from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
 app.config['MYSQL_HOST'] = 'custom-mysql.gamification.svc.cluster.local'
 app.config['MYSQL_USER'] = 'xxuser'
 app.config['MYSQL_PASSWORD'] = 'welcome1'
 app.config['MYSQL_DB']= 'sampledb'
+
 mysql = MySQL(app)
-@app.route('/style')
+
+@app.route('/', methods=['GET'])
 def employees():
 	cur = mysql.connection.cursor()
-	res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM sampledb.XXIBM_PRODUCT_STYLE LIMIT 10")
+	res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM sampledb.XXIBM_PRODUCT_STYLE")
 	if res > 0:
 		userDetails = cur.fetchall()
 		return render_template('employee.html',userDetails=userDetails)
+	else
+		return '''<h1>Database Information Missing</h1>
+<p>A prototype API </p>'''
+	
 		
 if __name__ == "__main__":
     app.run()
