2963492a2bf4d6195a588ba4173a6c42f4570cc2
ranipm27 <57614108+ranipm27@users.noreply.github.com>
2019-12-16 13:50:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,27 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config["MYSQL_HOST"] = 'custom-mysql.gamification.svc.cluster.local'
-app.config["MYSQL_USER"] = 'xxuser'
-app.config["MYSQL_PASSWORD"] = 'welcome1'
-app.config["MYSQL_DB"]= 'sampledb'
+mysql = MySQL()
 
-mysql = MySQL(app)
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'xxuser'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'welcome'
+app.config['MYSQL_DATABASE_DB'] = 'sampledb'
+app.config['MYSQL_DATABASE_HOST'] = 'custom-mysql.gamification.svc.cluster.local'
+mysql.init_app(app)
+
+#app.config["MYSQL_HOST"] = 'custom-mysql.gamification.svc.cluster.local'
+#app.config["MYSQL_USER"] = 'xxuser'
+#app.config["MYSQL_PASSWORD"] = 'welcome1'
+#app.config["MYSQL_DB"]= 'sampledb'
+#mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def employees():
-	cur = mysql.connection.cursor()
+	conn = mysql.connect()
+	cur = conn.cursor()
 	res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM sampledb.XXIBM_PRODUCT_STYLE")
-	if res > 0:
+	if len(res) > 0:
 		userDetails = cur.fetchall()
 		return render_template('employee.html',userDetails=userDetails)
 		
